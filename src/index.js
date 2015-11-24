e9c7d67edd63efddd849d7d43725b553ef10b909
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2015-11-24 11:00:27 -0600

    Imported SVG works (not right size)

--- src/index.js
+++ src/index.js
@@ -2,21 +2,38 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const rx = require('rx');
+const fs = require('fs');
 const appRoot = require('app-root-path');
 const Tail = require('tail').Tail;
 const ipcRenderer = require('electron').ipcRenderer;
 
-// $('#display').setAttribute('src', appRoot + '/images/example1.svg');
+const drawSVGFile = (drawing, path) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      console.log('Error reading ' + path, err);
+    } else {
+      drawing.svg(data);
+      console.log('Drew svg data');
+    }
+  });
+};
 
 const adjust = (opacity, channel) =>
   Array.from($$('.channel' + channel))
     .forEach(e => e.style.opacity = opacity);
 
-ipcRenderer.on('targetLog', (e, targetLog) => {
-  const tail = rx.Observable.fromEvent(new Tail(targetLog), 'line');
+const main = () => {
+  const drawing = SVG('main');
+  drawSVGFile(drawing, appRoot + '/images/example1.svg');
 
-  tail.map(line => line.split(' '))
-    .map(splitLine => splitLine.map(num => parseInt(num, 16)))
-    .tap(opacityValues => console.log(opacityValues))
-    .subscribe(opacityValues => opacityValues.forEach(adjust));
-});
+  // ipcRenderer.on('targetLog', (e, targetLog) => {
+  //   const tail = rx.Observable.fromEvent(new Tail(targetLog), 'line');
+
+  //   tail.map(line => line.split(' '))
+  //     .map(splitLine => splitLine.map(num => parseInt(num, 16)))
+  //     .tap(opacityValues => console.log(opacityValues))
+  //     .subscribe(opacityValues => opacityValues.forEach(adjust));
+  // });
+};
+
+main();
