16eddf760c5925eb2fce8a1c2faeff1c36ef610a
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2015-11-24 09:54:49 -0600

    cleaned up

--- src/index.js
+++ src/index.js
@@ -6,24 +6,18 @@ const appRoot = require('app-root-path');
 const Tail = require('tail').Tail;
 const ipcRenderer = require('electron').ipcRenderer;
 
-$('#display').setAttribute('src', appRoot + '/images/example1.svg');
+// $('#display').setAttribute('src', appRoot + '/images/example1.svg');
 
-adjust = (channel, opacity) => {
-  elements = $$('.channel' + channel)
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].style.opacity = opacity;
-  };
-};
+adjust = (channel, opacity) =>
+  $$('.channel' + channel).forEach(e => e.style.opacity = opacity);
 
 ipcRenderer.on('targetLog', (e, targetLog) => {
   const tail = rx.Observable.fromEvent(new Tail(targetLog), 'line');
 
   tail.map(line => line.split(' '))
     .map(splitLine => splitLine.map(num => parseInt(num, 16)))
-    .subscribe(channels => {
-      console.log(channels);
-      for (var i = 0; i < channels.length; i++) {
-        adjust(i, channels[i]);
-      };
-    });
+    .tap(channelValues => console.log(channelValues))
+    .subscribe(channelValues =>
+      channelValues.forEach((channelValue, channel) => adjust(channel, channelValue)));
+    
 });
