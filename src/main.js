b13c79a12c516834704936758845283c6b0a1b63
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2015-11-23 20:33:35 -0600

    Cleaned up main.js code

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
-const app = require('app');  // Module to control application life.
-const BrowserWindow = require('browser-window');  // Module to create native browser window.
-const appRoot = require('app-root-path')
+const app = require('app');
+const BrowserWindow = require('browser-window');
+const appRoot = require('app-root-path');
 
 var mainWindow = null;
 
-app.on('window-all-closed', () => {
-  app.quit();
-});
+app.on('window-all-closed', () => app.quit());
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({width: 800, height: 600});
-
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+  mainWindow.loadURL('file://' + appRoot + '/images/example1.svg');
+  mainWindow.on('closed', () => mainWindow = null);
 });
