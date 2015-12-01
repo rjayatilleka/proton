92581a66aae764011b1834f09c8b25b205a51710
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2015-11-30 21:13:55 -0600

    Added Babel with es2015-node5

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,42 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename')
-// const babel = require('gulp-babel')
+const babel = require('gulp-babel')
 const del = require('del')
 const spawn = require('child_process').spawn
 const argv = require('minimist')(process.argv.slice(2))
 
-gulp.task('build-src', ['clean'], () =>
+gulp.task('default', [
+  'build-src',
+  'build-lib',
+  'build-templates',
+  'build-config',
+  'build-layout'
+])
+
+gulp.task('clean', () =>
+  del(['build', 'dist']))
+
+gulp.task('build-src', () =>
   gulp.src('src/*')
+    .pipe(babel({
+      presets: ['es2015-node5']
+    }))
     .pipe(gulp.dest('build')))
 
-gulp.task('build-lib', ['clean'], () =>
+gulp.task('build-lib', () =>
   gulp.src('lib/*')
     .pipe(gulp.dest('build')))
 
-gulp.task('build-config', ['clean'], () =>
+gulp.task('build-templates', () =>
+  gulp.src('templates/*')
+    .pipe(gulp.dest('build')))
+
+gulp.task('build-config', () =>
   gulp.src(argv.config)
     .pipe(rename('config.yaml'))
     .pipe(gulp.dest('build')))
 
-gulp.task('build-layout', ['clean'], () =>
+gulp.task('build-layout', () =>
   gulp.src(argv.layout)
     .pipe(rename('layout.svg'))
     .pipe(gulp.dest('build')))
-
-gulp.task('default', ['build-src', 'build-lib', 'build-config', 'build-layout'])
-
-gulp.task('clean', () =>
-  del(['build', 'dist']))
