2c0b2610df63ddcb8f8b020fc4b03ae2ab0d0d17
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2015-12-26 22:13:37 -0600

    semicolons

--- src/main.js
+++ src/main.js
@@ -1,24 +1,29 @@
 import yaml from 'js-yaml'
+
 import fs from 'fs'
 import {app, BrowserWindow} from 'electron'
 
-let mainWindow = null
+let mainWindow = null;
 
-app.on('window-all-closed', () => app.quit())
+function dummyFunc(helloMsg, logger) {
+  logger.log(helloMsg);
+  console.log('Logged!');
+}
+
+app.on('window-all-closed', () => app.quit());
 
 app.on('ready', () => {
   fs.readFile('./build/config.yaml', 'utf8', (err, data) => {
-    const config = yaml.safeLoad(data)
+    const config = yaml.safeLoad(data);
 
     // Window set up
-    mainWindow = new BrowserWindow(config.size)
-    mainWindow.loadURL('file://' + __dirname + '/index.html')
-    mainWindow.on('closed', () => mainWindow = null)
+    mainWindow = new BrowserWindow(config.size);
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.on('closed', () => mainWindow = null);
 
-    const webContents = mainWindow.webContents
-    webContents.openDevTools()
+    const webContents = mainWindow.webContents;
+    webContents.openDevTools();
 
-    webContents.on('did-finish-load', () =>
-      webContents.send('config', config))
-  })
-})
+    webContents.on('did-finish-load', () => webContents.send('config', config))
+  });
+});
